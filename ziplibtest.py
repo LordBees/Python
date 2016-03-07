e3c1afe23159c98cdb7cdf1eb64f61e1a1490b30
LordBees <LordBees@users.noreply.github.com>
2016-03-07 13:20:00 +0000

    Added files via upload

---
+++ ziplibtest.py
@@ -0,0 +1,25 @@
+#ziplib
+
+##def unzip(source_filename, dest_dir):
+##    with zipfile.ZipFile(source_filename) as zf:
+##        for member in zf.infolist():
+##            zf.extract(member, dest_dir)
+
+##def unzip(source_filename, dest_dir):
+##    with zipfile.ZipFile(source_filename) as zf:
+##        zf.extractall(dest_dir)
+
+##import zipfile
+##with zipfile.ZipFile("ds_dphs_csv.zip") as a:
+##        a.extractall()
+import zipfile,os
+def ext2dir(fname,dname):
+    zipfl = zipfile.ZipFile(os.getcwd()+'\\'+fname+".zip")
+    os.mkdir(str(dname))
+    os.chdir(str(dname))
+#    with  as a:
+    zipfl.extractall()
+        
+def extract_all():
+    with zipfile.ZipFile("ds_dphs_csv.zip") as a:
+        a.extractall()
